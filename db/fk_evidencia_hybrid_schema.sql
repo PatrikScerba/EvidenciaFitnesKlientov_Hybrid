4b4c64bba8eb0875463ca75cb9fbc6ff6872833e
Patrik Scerba <patrikscerba@gmail.com>
2025-12-10 16:31:41 +0100

    chore(db): pridaná SQL schéma databázy a tabuľky klient

---
+++ db/fk_evidencia_hybrid_schema.sql
@@ -0,0 +1,66 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Hostiteľ: 127.0.0.1
+-- Čas generovania: St 10.Dec 2025, 16:10
+-- Verzia serveru: 10.4.32-MariaDB
+-- Verzia PHP: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Databáza: `fk_evidencia_hybrid`
+--
+CREATE DATABASE IF NOT EXISTS `fk_evidencia_hybrid` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
+USE `fk_evidencia_hybrid`;
+
+-- --------------------------------------------------------
+
+--
+-- Štruktúra tabuľky pre tabuľku `klienti`
+--
+
+CREATE TABLE `klienti` (
+  `id` int(11) NOT NULL,
+  `krstne_meno` varchar(50) DEFAULT NULL,
+  `priezvisko` varchar(50) DEFAULT NULL,
+  `datum_narodenia` date DEFAULT NULL,
+  `telefonne_cislo` varchar(15) DEFAULT NULL,
+  `email` varchar(100) DEFAULT NULL,
+  `adresa` varchar(255) DEFAULT NULL,
+  `datum_registracie` date DEFAULT curdate()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Kľúče pre exportované tabuľky
+--
+
+--
+-- Indexy pre tabuľku `klienti`
+--
+ALTER TABLE `klienti`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT pre exportované tabuľky
+--
+
+--
+-- AUTO_INCREMENT pre tabuľku `klienti`
+--
+ALTER TABLE `klienti`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
