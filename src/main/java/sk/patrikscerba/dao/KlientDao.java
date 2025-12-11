9076d59706fc45e75938b086ba8269d0e0367018
Patrik Scerba <patrikscerba@gmail.com>
2025-12-11 18:08:05 +0100

    (dao): pridaný model Klient a rozhranie KlientDao pre CRUD operácie

--- src/main/java/sk/patrikscerba/dao/KlientDao.java
+++ src/main/java/sk/patrikscerba/dao/KlientDao.java
@@ -1,4 +1,20 @@
 package sk.patrikscerba.dao;
 
+import sk.patrikscerba.model.Klient;
+import java.util.List;
+
 public interface KlientDao {
+
+    int ulozKlienta(Klient klient);
+
+
+    Klient najdiKlientaPodlaId(int id);
+
+    List<Klient> ziskajVsetkychKlientov();
+
+    boolean aktualizujKlienta(Klient klient);
+
+    boolean vymazatKlienta(int id);
+
+
 }
--- src/main/java/sk/patrikscerba/model/Klient.java
+++ src/main/java/sk/patrikscerba/model/Klient.java
@@ -1,4 +1,101 @@
 package sk.patrikscerba.model;
 
+import java.time.LocalDate;
+
+
 public class Klient {
+
+    private int id;
+    private String krstneMeno;
+    private String priezvisko;
+    private LocalDate datumNarodenia;
+    private String telefonneCislo;
+    private String adresa;
+    private String email;
+    private LocalDate datumRegistracie;
+
+
+    public Klient(){
+    }
+
+    // konštruktor pre databázu
+    public Klient(int id, String krstneMeno, String priezvisko, LocalDate datumNarodenia,
+                  String telefonneCislo, String adresa, String email, LocalDate datumRegistracie) {
+        this.id = id;
+        this.krstneMeno = krstneMeno;
+        this.priezvisko = priezvisko;
+        this.datumNarodenia = datumNarodenia;
+        this.telefonneCislo = telefonneCislo;
+        this.adresa = adresa;
+        this.email = email;
+        this.datumRegistracie = datumRegistracie;
+
+    }
+
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getKrstneMeno() {
+        return krstneMeno;
+    }
+
+    public void setKrstneMeno(String krstneMeno) {
+        this.krstneMeno = krstneMeno;
+    }
+
+    public String getPriezvisko() {
+        return priezvisko;
+    }
+
+    public void setPriezvisko(String priezvisko) {
+        this.priezvisko = priezvisko;
+    }
+
+    public LocalDate getDatumNarodenia() {
+        return datumNarodenia;
+    }
+
+    public void setDatumNarodenia(LocalDate datumNarodenia) {
+        this.datumNarodenia = datumNarodenia;
+    }
+
+    public String getTelefonneCislo() {
+        return telefonneCislo;
+    }
+
+    public void setTelefonneCislo(String telefonneCislo) {
+        this.telefonneCislo = telefonneCislo;
+    }
+
+    public String getAdresa() {
+        return adresa;
+    }
+
+    public void setAdresa(String adresa) {
+        this.adresa = adresa;
+    }
+
+    public LocalDate getDatumRegistracie() {
+        return datumRegistracie;
+    }
+
+    public void setDatumRegistracie(LocalDate datumRegistracie) {
+        this.datumRegistracie = datumRegistracie;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
 }
+
+
