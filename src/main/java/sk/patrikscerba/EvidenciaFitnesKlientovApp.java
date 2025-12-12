2dbb3d1e63791cde0ad2591d9d32a5d2ac88b850
Patrik Scerba <patrikscerba@gmail.com>
2025-12-12 18:45:59 +0100

    feat: implementácia základných CRUD operácií pre KlientDao (manuálne testovanie v spúšťacej triede)

--- src/main/java/sk/patrikscerba/EvidenciaFitnesKlientovApp.java
+++ src/main/java/sk/patrikscerba/EvidenciaFitnesKlientovApp.java
@@ -1,7 +1,12 @@
 package sk.patrikscerba;
 
 import sk.patrikscerba.dao.DatabazaPripojenie;
+import sk.patrikscerba.dao.KlientDao;
+import sk.patrikscerba.dao.KlientDaoImpl;
+import sk.patrikscerba.model.Klient;
 import java.sql.Connection;
+import java.time.LocalDate;
+import java.util.List;
 
 public class EvidenciaFitnesKlientovApp {
     public static void main(String[] args) {
@@ -20,5 +25,85 @@ public class EvidenciaFitnesKlientovApp {
         }catch (Exception e ){
             System.out.println("chyba pri pripájaní " + e.getMessage());
         }
+
+        // otestovanie CRUD operácií
+        KlientDao klientDao = new KlientDaoImpl();
+
+        /*
+        ------  otestovanie uloženie klienta do databázy ------------
+
+        Klient novyKlient = new Klient();
+
+        novyKlient.setKrstneMeno("PatrikTest");
+        novyKlient.setPriezvisko("ScerbaTest");
+        novyKlient.setDatumNarodenia(LocalDate.of(1990, 1, 15));
+        novyKlient.setTelefonneCislo("0900123456");
+        novyKlient.setAdresa("Kosice 1");
+        novyKlient.setEmail("test@patrik.sk");
+
+        novyKlient.setKrstneMeno("PatrikTest2");
+        novyKlient.setPriezvisko("ScerbaTest2");
+        novyKlient.setDatumNarodenia(LocalDate.of(1990, 1, 15));
+        novyKlient.setTelefonneCislo("0900123456");
+        novyKlient.setAdresa("Kosice 1");
+        novyKlient.setEmail("test@patrik.sk");
+
+        int id = klientDao.ulozKlienta(novyKlient);
+        System.out.println("Uložené ID: " + id);
+
+       ------- otetovanie načítanie klienta z databázy -------
+
+       Klient nacitanyKlient = klientDao.najdiKlientaPodlaId(id);
+       System.out.println("Načítaný klient: "
+              + nacitanyKlient.getId() + " | "
+              + nacitanyKlient.getKrstneMeno() + " "
+              + nacitanyKlient.getPriezvisko() + " | "
+              + nacitanyKlient.getEmail());
+
+        ------- otestovanie načítania všetkých klientov z databázy ------
+
+        List<Klient>klienti = klientDao.ziskajVsetkychKlientov();
+
+        System.out.println("Počet klientov:" + klienti.size());
+
+        for (Klient klient : klienti){
+            System.out.println(
+                    klient.getId() + " |" +
+                    klient.getKrstneMeno() + " " +
+                    klient.getPriezvisko() + " " +
+                    klient.getEmail() + " " +
+                    klient.getDatumRegistracie()
+            );
+        }
+
+        ------ otestovanie aktualizovanie klienta a uloženie do databázy -------
+
+        Klient klient = klientDao.najdiKlientaPodlaId(3); // existujúce ID
+
+        if (klient == null) {
+            System.out.println("Klient neexistuje");
+            return;
+        }
+        klient.setTelefonneCislo("0911222333");
+        klient.setEmail("update@test.sk");
+        klient.setAdresa("Michalovce – update");
+
+        boolean uspesne = klientDao.aktualizujKlienta(klient);
+
+        System.out.println(uspesne
+                ? "Klient bol úspešne aktualizovaný"
+                : "Aktualizácia zlyhala");
+
+        ------ otestovanie vymazanie klienta z databázy -------
+
+        int idVymazanieKlienta = 7;
+        int klientVymazany = idVymazanieKlienta;
+
+        boolean vymazanie = klientDao.vymazatKlienta(idVymazanieKlienta);
+        System.out.println(vymazanie ? "Klient s Id" + " " + klientVymazany + " " + "sa vymazal!" :
+                "klient neexistuje alebo bol vymazaný! ");
+    */
+
     }
 }
+
--- src/main/java/sk/patrikscerba/dao/KlientDaoImpl.java
+++ src/main/java/sk/patrikscerba/dao/KlientDaoImpl.java
@@ -1,4 +1,192 @@
 package sk.patrikscerba.dao;
 
-public class KlientDaoImpl {
+import sk.patrikscerba.model.Klient;
+
+import java.sql.Connection;
+import java.sql.Date;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
+
+//Trieda zabezpečuje databázové operácie nad klientmi a mapovanie databázových dát na objekt Klient.
+public class KlientDaoImpl implements KlientDao {
+
+
+    private final DatabazaPripojenie databazaPripojenie;
+
+    // Konštruktor - inicializuje pripojenie k databáze
+    public KlientDaoImpl() {
+        this.databazaPripojenie = new DatabazaPripojenie();
+    }
+
+    // Uloží nového klienta do DB
+    @Override
+    public int ulozKlienta(Klient klient) {
+
+        //Použitie PreparedStatement kvôli bezpečnosti ( SQL injection )
+        String sql = """
+                INSERT INTO klienti (krstne_meno, priezvisko, datum_narodenia, telefonne_cislo, adresa, email)
+                VALUES (?, ?, ?, ?, ?, ?)
+                """;
+        //try-with-resources = Connection aj PreparedStatement s automatický zatvoria
+        try (Connection connection = databazaPripojenie.getConnection();
+             PreparedStatement preparedStatement = connection.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS)) {
+
+            //Napĺnenie hodnôt do SQL podľa poradia v INSERT
+            preparedStatement.setString(1, klient.getKrstneMeno());
+            preparedStatement.setString(2, klient.getPriezvisko());
+            preparedStatement.setDate(3, Date.valueOf(klient.getDatumNarodenia()));
+            preparedStatement.setString(4, klient.getTelefonneCislo());
+            preparedStatement.setString(5, klient.getAdresa());
+            preparedStatement.setString(6, klient.getEmail());
+
+
+            preparedStatement.executeUpdate();
+
+            // Vracia vygenerované ID z databázy po INSERT-e (AUTO_INCREMENT)
+            try (ResultSet resultSet = preparedStatement.getGeneratedKeys()) {
+                if (resultSet.next()) {
+                    return resultSet.getInt(1);
+                }
+            }
+
+            //Ak sa ID nepodarilo získať, vrátime -1 (signalizuje problém)
+            return -1;
+
+        } catch (SQLException e) {
+            throw new RuntimeException("Chyba pri ukladaní klienta do databázy: " + e.getMessage(), e);
+        }
+    }
+
+    // Nájde klienta podľa ID (ak neexistuje, vráti null)
+    @Override
+    public Klient najdiKlientaPodlaId(int id) {
+
+        String sql = "SELECT * FROM klienti WHERE id = ?";
+
+        try (Connection connection = databazaPripojenie.getConnection();
+             PreparedStatement preparedStatement = connection.prepareStatement(sql)) {
+
+
+            preparedStatement.setInt(1, id);
+
+            try (ResultSet resultSet = preparedStatement.executeQuery()) {
+                if (resultSet.next()) {
+                    return mapujKlientaZResultSetu(resultSet);
+                }
+            }
+
+            return null;
+
+        } catch (SQLException e) {
+            throw new RuntimeException("Chyba pri hľadaní klienta podľa ID: " + e.getMessage(), e);
+        }
+    }
+
+    // Načíta všetkých klientov (zoradené podľa dátumu registrácie od najnovších)
+    @Override
+    public List<Klient> ziskajVsetkychKlientov() {
+
+        String sql = "SELECT * FROM klienti ORDER BY datum_registracie DESC";
+        List<Klient> klienti = new ArrayList<>();
+
+        try (Connection connection = databazaPripojenie.getConnection();
+             PreparedStatement preparedStatement = connection.prepareStatement(sql);
+             ResultSet resultSet = preparedStatement.executeQuery()) {
+
+            //Prejde všetky riadky a každý zmapuje na objekt
+            while (resultSet.next()) {
+                klienti.add(mapujKlientaZResultSetu(resultSet));
+            }
+
+            return klienti;
+
+        } catch (SQLException e) {
+            throw new RuntimeException("Chyba pri načítaní všetkých klientov: " + e.getMessage(), e);
+        }
+    }
+
+    // Aktualizuje existujúceho klienta podľa ID (vracia true/false podľa toho, či sa niečo zmenilo)
+    @Override
+    public boolean aktualizujKlienta(Klient klient) {
+
+        String sql = """
+                UPDATE klienti SET
+                    krstne_meno = ?,
+                    priezvisko = ?,
+                    datum_narodenia = ?,
+                    telefonne_cislo = ?,
+                    email = ?,
+                    adresa = ?
+                WHERE id = ?
+                """;
+
+        try (Connection connection = databazaPripojenie.getConnection();
+             PreparedStatement preparedStatement = connection.prepareStatement(sql)) {
+
+            //Napĺnenie hodnôt do UPDATE podľa poradia "?"
+            preparedStatement.setString(1, klient.getKrstneMeno());
+            preparedStatement.setString(2, klient.getPriezvisko());
+            preparedStatement.setDate(3, Date.valueOf(klient.getDatumNarodenia()));
+            preparedStatement.setString(4, klient.getTelefonneCislo());
+            preparedStatement.setString(5, klient.getEmail());
+            preparedStatement.setString(6, klient.getAdresa());
+            preparedStatement.setInt(7, klient.getId());
+
+            //Vráti počet zmenených riadkov
+            return preparedStatement.executeUpdate() > 0;
+
+        } catch (SQLException e) {
+            throw new RuntimeException("Chyba pri aktualizácii klienta: " + e.getMessage(), e);
+        }
+    }
+
+    // Vymaže klienta podľa ID
+    @Override
+    public boolean vymazatKlienta(int id) {
+
+        String sql = "DELETE FROM klienti WHERE id = ?";
+
+        try (Connection connection = databazaPripojenie.getConnection();
+             PreparedStatement preparedStatement = connection.prepareStatement(sql)) {
+
+            preparedStatement.setInt(1, id);
+
+            //Ak sa v databáze vymazal aspoň 1 riadok tak vymazanie prebehlo
+            return preparedStatement.executeUpdate() > 0;
+
+        } catch (SQLException e) {
+            throw new RuntimeException("Chyba pri mazaní klienta: " + e.getMessage(), e);
+        }
+    }
+
+    // Pomocná metóda: premení 1 riadok z ResultSetu na objekt Klient
+    private Klient mapujKlientaZResultSetu(ResultSet resultSet) throws SQLException {
+
+        // Povinné údaje z databázy
+        int id = resultSet.getInt("id");
+        String krstneMeno = resultSet.getString("krstne_meno");
+        String priezvisko = resultSet.getString("priezvisko");
+        LocalDate datumNarodenia = resultSet.getDate("datum_narodenia").toLocalDate();
+        String telefonneCislo = resultSet.getString("telefonne_cislo");
+        String email = resultSet.getString("email");
+        String adresa = resultSet.getString("adresa");
+
+        // Dátum registrácie je v databáze default (CURRENT_DATE)
+        //ale pre istotu je ošetrenie  null (ak by vrátila prázdnu hodnotu)
+        LocalDate datumRegistracie = null;
+        Date registracnyDatum = resultSet.getDate("datum_registracie");
+        if (registracnyDatum != null) {
+            datumRegistracie = registracnyDatum.toLocalDate();
+        }
+
+        // Objekt Klient s všetkými údajmi
+        return new Klient(id, krstneMeno, priezvisko, datumNarodenia, telefonneCislo, adresa, email, datumRegistracie);
+    }
 }
