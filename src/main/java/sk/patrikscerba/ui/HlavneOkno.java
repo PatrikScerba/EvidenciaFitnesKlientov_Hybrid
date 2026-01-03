bcbd36f206727b40b7635a82af0c5ca4aac29d3d
Patrik Scerba <patrikscerba@gmail.com>
2026-01-03 18:28:11 +0100

    feat(app): základné hlavné okno aplikácie + štart aplikácie

--- src/main/java/sk/patrikscerba/ui/HlavneOkno.java
+++ src/main/java/sk/patrikscerba/ui/HlavneOkno.java
@@ -1,4 +1,29 @@
 package sk.patrikscerba.ui;
 
-public class HlavneOkno {
+import javax.swing.*;
+
+public class HlavneOkno extends JFrame {
+
+    private JPanel mainPanel;
+    private JButton Registracia;
+    private JButton Vyhladanie;
+    private JButton Klienti;
+    private JLabel verzia3Label;
+    private JLabel developedByPatrikŠčerbaLabel;
+
+    //Nastavenie hlavného okna aplikácie.
+    public HlavneOkno(){
+
+        setContentPane(mainPanel);
+        setTitle("Evidencia klientov");
+        setSize(650, 250);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setLocationRelativeTo(null);
+        setVisible(true);
+
+        //Nastavenie akcií tlačidiel v hlavnom okne
+        Registracia.addActionListener(e -> new Registracia().setVisible(true));
+        Vyhladanie.addActionListener(e -> new Vyhladavanie().setVisible(true));
+        Klienti.addActionListener(e ->  new ZoznamKlientov().setVisible(true));
+    }
 }
