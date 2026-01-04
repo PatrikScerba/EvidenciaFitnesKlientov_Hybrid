067ee38382f04878253654531af747af764b2f7c
Patrik Scerba <patrikscerba@gmail.com>
2026-01-04 19:11:11 +0100

    feat(app): detekcia DB pri štarte + nastavenie online/offline režimu

--- src/main/java/sk/patrikscerba/EvidenciaFitnesKlientovApp.java
+++ src/main/java/sk/patrikscerba/EvidenciaFitnesKlientovApp.java
@@ -1,26 +1,45 @@
 package sk.patrikscerba;
 
 import com.formdev.flatlaf.FlatLightLaf;
+import sk.patrikscerba.io.db.DatabazaPripojenie;
+import sk.patrikscerba.io.log.AppLogServis;
 import sk.patrikscerba.ui.HlavneOkno;
 import javax.swing.*;
 
+
 public class EvidenciaFitnesKlientovApp {
+
+    private static final AppLogServis applog = new AppLogServis();
 
     public static void main(String[] args) {
 
-        // Nastavenie moderného vzhľadu FlatLaf
-        try {
-            UIManager.setLookAndFeel(new FlatLightLaf());
-        } catch (Exception e) {
-            System.err.println("Nepodarilo sa načítať FlatLaf: " + e.getMessage());
-        }
+        boolean dostupnaDatabaza = DatabazaPripojenie.testConnection();
 
-        // Spustenie GUI na správnom vlákne (AWT)
-        SwingUtilities.invokeLater(() ->
-            new HlavneOkno().setVisible(true)
-        );
+        if (!dostupnaDatabaza) {
+            sk.patrikscerba.system.SystemRezim.setOffline(true);
+
+            applog.info("Databáza nedostupná. Aplikácia spustená v OFFLINE režime.");
+
+        } else {
+            sk.patrikscerba.system.SystemRezim.setOffline(false);
+
+            applog.info("Databáza dostupná. Aplikácia spustená v ONLINE režime.");
+
+            // Nastavenie moderného vzhľadu FlatLaf
+            try {
+                UIManager.setLookAndFeel(new FlatLightLaf());
+            } catch (Exception e) {
+                applog.error("Nepodarilo sa načítať FlatLaf: ", e);
+            }
+
+            // Spustenie GUI na správnom vlákne (AWT)
+            SwingUtilities.invokeLater(() ->
+                    new HlavneOkno().setVisible(true)
+            );
+        }
     }
 }
+
 
 
 
