f6b94cc9b09c01d79a4d2f7c5973689068166827
Patrik Scerba <patrikscerba@gmail.com>
2026-01-06 13:08:14 +0100

    feat(ui): základ formulára registrácie klienta(bez logiky)

--- src/main/java/sk/patrikscerba/ui/Registracia.java
+++ src/main/java/sk/patrikscerba/ui/Registracia.java
@@ -2,10 +2,27 @@ package sk.patrikscerba.ui;
 
 import javax.swing.*;
 
+// Okno pre registráciu nového klienta
 public class Registracia extends JFrame {
+
+    private JButton buttonRegistrovat;
+    private JTextField jTextKrstneMeno;
+    private JTextField jTextPriezvisko;
+    private JTextField jTextDatumNarodenia;
+    private JTextField jTextTelefonneCislo;
+    private JTextField jTextAdresa;
+    private JTextField jTextEmail;
+
+    private JLabel krstneMenoLabel;
+    private JLabel priezviskoLabel;
+    private JLabel datumNarodeniaLabel;
+    private JLabel telefonneCisloLabel;
+    private JLabel adresaLabel;
+    private JLabel emailLabel;
 
     private JPanel mainPanel;
 
+    // Konštruktor okna pre registráciu klienta
     public Registracia() {
         setContentPane(mainPanel);
         setTitle("Registrácia klienta");
@@ -13,6 +30,29 @@ public class Registracia extends JFrame {
         setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
         setLocationRelativeTo(null);
         setVisible(true);
+
+        // Pridanie akcie na tlačidlo registrácie
+        buttonRegistrovat.addActionListener(e -> registrujKlienta());
+    }
+        private void registrujKlienta() {
+            String krstneMeno = jTextKrstneMeno.getText();
+            String priezvisko = jTextPriezvisko.getText();
+            String datumNarodenia = jTextDatumNarodenia.getText();
+            String telefonneCislo = jTextTelefonneCislo.getText();
+            String adresa = jTextAdresa.getText();
+            String email = jTextEmail.getText();
+
+            //------------Otestovanie výpisu údajov klienta do konzoly-------------
+            System.out.println("Registrujem klienta: \n" + krstneMeno + " " + priezvisko +
+                    ", \nDátum narodenia: " + datumNarodenia +
+                    ", \nTelefón: " + telefonneCislo +
+                    ", \nAdresa: " + adresa +
+                    ", \nEmail: " + email +
+                    "\n=============================" +
+                    "\nKlient zaregistrovaný úspešne.");
     }
 }
+
+
+
 
