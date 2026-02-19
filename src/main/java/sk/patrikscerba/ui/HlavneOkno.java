8dc87b0b010ac23ba5b8d8c806ba86d0e416855e
Patrik Scerba <patrikscerba@gmail.com>
2026-02-19 20:11:05 +0100

    fix(ui): doplnenie triedy HlavneOkno k formu

--- src/main/java/sk/patrikscerba/ui/HlavneOkno.java
+++ src/main/java/sk/patrikscerba/ui/HlavneOkno.java
@@ -1,27 +1,45 @@
 package sk.patrikscerba.ui;
 
 import javax.swing.*;
+import java.awt.*;
 
 public class HlavneOkno extends JFrame {
 
     private JPanel mainPanel;
-    private JButton Registracia;
-    private JButton Vyhladanie;
-    private JButton Klienti;
-    private JButton HistoriaVstupov;
-    private JButton Scanner;
-    private JButton SystemLog;
-    private JLabel verzia3Label;
+    private JButton registracia;
+    private JButton vyhladanie;
+    private JButton klienti;
+    private JButton historiaVstupov;
+    private JButton scanner;
+    private JButton systemLog;
     private JLabel developedByPatrikŠčerbaLabel;
+
+    private JLabel hlavnyNadpisLabel;
+    private JLabel podnadpisLabel;
+
+    private JPanel navigaciaPanel1;
+    private JPanel navigaciaPanel2;
+    private JPanel nazovPanel;
+    private JPanel logoPanel;
+    private JLabel logoLabel;
+    private JPanel Stlpec;
+    private JPanel panelPozadie;
+
 
     //Nastavenie hlavného okna aplikácie.
     public HlavneOkno() {
 
         setContentPane(mainPanel);
         setTitle("Evidencia klientov");
-        setSize(650, 250);
+        setSize(670, 350);
+        setResizable(false);
         setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         setLocationRelativeTo(null);
+
+        logoLabel.setIcon(vytvorLogoIkonu());
+
+        logoPanel.revalidate();
+        logoPanel.repaint();
 
         nastavRezim();
         nastavAkcieTlacidiel();
@@ -32,23 +50,32 @@ public class HlavneOkno extends JFrame {
     // Nastavenie režimu systému (online/offline) a povolení pre tlačidlá v hlavnom okne
     private void nastavRezim() {
         if (sk.patrikscerba.system.SystemRezim.isOffline()) {
-            Registracia.setEnabled(false);
-            Klienti.setEnabled(true);
-            Vyhladanie.setEnabled(true);
-            HistoriaVstupov.setEnabled(true);
-            Scanner.setEnabled(true);
-            SystemLog.setEnabled(true);
+            registracia.setEnabled(false);
+            klienti.setEnabled(true);
+            vyhladanie.setEnabled(true);
+            historiaVstupov.setEnabled(true);
+            scanner.setEnabled(true);
+            systemLog.setEnabled(true);
         }
     }
 
     //Nastavenie akcií tlačidiel v hlavnom okne
     private void nastavAkcieTlacidiel() {
-        Registracia.addActionListener(e -> new Registracia().setVisible(true));
-        Vyhladanie.addActionListener(e -> new Vyhladavanie(false).setVisible(true));
-        Klienti.addActionListener(e -> new ZoznamKlientov().setVisible(true));
-        HistoriaVstupov.addActionListener(e -> new HistoriaVstupov().setVisible(true));
-        Scanner.addActionListener(e -> new ScannerQrOkno().setVisible(true));
-        SystemLog.addActionListener(e -> new SystemLogOkno().setVisible(true));
+        registracia.addActionListener(e -> new Registracia().setVisible(true));
+        vyhladanie.addActionListener(e -> new Vyhladavanie(false).setVisible(true));
+        klienti.addActionListener(e -> new ZoznamKlientov().setVisible(true));
+        historiaVstupov.addActionListener(e -> new HistoriaVstupov().setVisible(true));
+        scanner.addActionListener(e -> new ScannerQrOkno().setVisible(true));
+        systemLog.addActionListener(e -> new SystemLogOkno().setVisible(true));
+    }
+
+    private ImageIcon vytvorLogoIkonu() {
+        java.net.URL url = getClass().getResource("/obrazok/Logo.png");
+        if (url == null) return null;
+
+        ImageIcon icon = new ImageIcon(url);
+        Image scaled = icon.getImage().getScaledInstance(280, 200, Image.SCALE_SMOOTH);
+
+        return new ImageIcon(scaled);
     }
 }
-
